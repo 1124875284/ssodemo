2d55cf41b45c83683d127bca9d62a307df0a4294
huangzhiqiang <qq1124875284@163.com>
2019-03-26 16:52:26 +0800

    Initial commit

---
+++ sso-server/src/main/java/com/hzq/SsoServerApplication.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.hzq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author huangzhiqiang
+ *
+ */
+@SpringBootApplication
+public class SsoServerApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SsoServerApplication.class, args);
+	}
+
+}
